1fa19bd8bd8dedb2a23c32eb343727a1752b01fd
Punkrockechidna <dan.kaplan@ieleadership.org>
2020-02-15 23:16:58 -0500

    finsihed the backend api

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '5ccff478176b4d8eb5008a54e66c4c75'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to get etries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
